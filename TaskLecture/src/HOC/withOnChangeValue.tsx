5b836bf5baecfa9ef157c19fc2684be2ec9e9d11
RomanVr <romakf99@yandex.ru>
2021-10-02 13:35:15 +0300

    the solution of the task

--- TaskLecture/src/HOC/withOnChangeValue.tsx
+++ TaskLecture/src/HOC/withOnChangeValue.tsx
@@ -4,14 +4,28 @@ import React from "react";
  * см тесты в src/utils/withOnChangeValue.test.tsx
  * @param Component
  */
-export const withOnChangeValue =
-  <P extends object>(Component: React.ComponentType<P>) =>
-  (
+export const withOnChangeValue = <P extends Record<string, unknown>>(
+  Component: React.ComponentType<P>
+) => {
+  const WithOnChangeValue = (
     props: P & {
       onChange?: (ev: React.ChangeEvent) => void;
       onChangeValue?: (newText: string) => void;
     }
   ) => {
     // put your code here
-    return null;
+    const onChange = (ev: React.ChangeEvent<Element>) => {
+      if (props.onChangeValue) {
+        props.onChangeValue((ev.target as HTMLInputElement).value);
+      }
+      if (props.onChange) {
+        props.onChange(ev);
+      }
+    };
+    return <Component onChange={onChange} />;
   };
+
+  WithOnChangeValue.displayName = `${Component.displayName || Component}`;
+
+  return WithOnChangeValue;
+};
