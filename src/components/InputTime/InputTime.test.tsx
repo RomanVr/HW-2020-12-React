01e855bc861f863e384f1a7f49dbd89b0f68edd9
RomanVr <romakf99@yandex.ru>
2021-10-31 17:40:08 +0300

    fix test inputTime

--- src/components/InputTime/InputTime.test.tsx
+++ src/components/InputTime/InputTime.test.tsx
@@ -2,21 +2,21 @@ import React from "react";
 import { act, render, screen } from "@testing-library/react";
 import { InputTime } from "./InputTime";
 
+// const sleep = (x: number) => new Promise((resolve) => setTimeout(resolve, x));
+
 describe("Test InputTime", () => {
-  beforeEach(() => {
-    render(<InputTime />);
+  it("Render input for with value Time", async () => {
     jest.useFakeTimers();
-  });
-  it("Render input for with value Time", () => {
-    act(() => {
-      jest.advanceTimersByTime(500);
+    render(<InputTime />);
+
+    await act(async () => {
+      jest.advanceTimersByTime(1000);
     });
-    expect(screen.getByTestId("InputText")).toBeInTheDocument();
-  });
-  it("Render input for with value Time", () => {
-    act(() => {
-      jest.advanceTimersByTime(15000);
+    expect(screen.getByDisplayValue("00:00:01")).toBeInTheDocument();
+
+    await act(async () => {
+      jest.advanceTimersByTime(11000);
     });
-    expect(screen.getByTestId("InputText")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("00:00:12")).toBeInTheDocument();
   });
 });
